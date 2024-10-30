5fcbbd7d4a395c8656abc39ad1411dc83d76df77
Yery Alexander Rodriguez Gonzalez <yeryalex03@gmail.com>
2024-10-30 19:45:54 +0100

    ex17

---
+++ ex17/ft_strcmp.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yrodrigu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/30 19:32:14 by yrodrigu          #+#    #+#             */
+/*   Updated: 2024/10/30 19:45:31 by yrodrigu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include <stdio.h>
+#include <string.h>
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+	return ((unsigned char)*s1 - (unsigned char)*s2);
+}
+int main()
+{
+	printf("%i\n", ft_strcmp("ho", "hol"));
+	printf("%i\n", strcmp("ho", "hol"));
+	return (0);
+}
